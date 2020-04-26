fa4c606799bdeea291c330d96e6baa38f250a50d
Adriano Almeida <adrianoalmeida@MacBook-Pro-de-Adriano.local>
2020-04-25 21:00:22 -0300

    Simulando uma falha no arquivo falha_test e configurando o Jenkinsfile para emitir um relatório indenpente de sucesso ou erros

---
+++ tests/busca/test_falha.test.js
@@ -0,0 +1,34 @@
+
+
+
+module.exports = {
+
+    '@tags': ['smoke', '404'],
+
+    before: function (browser) {
+        
+
+        let login = browser.page.login()
+        let sidebar = browser.page.sidebar()
+
+        login
+            .with('didico@ninjazombie.com', 'pwd123')
+        sidebar
+            .expectLoggedUser('Adriano Almeida')
+    },
+
+    'quando eu busco um título não cadastrado': function (browser) {
+        let movie = browser.page.movie()
+
+        movie
+            .setValue('@searchInput', 'Não é mais um besteirol americano')
+            .click('@searchIcon')
+    },
+
+    'então devo ver uma mensagem de alerta': function (browser) {
+        let movie = browser.page.movie()
+        movie
+            .waitForElementPresent('@alertDanger', 10000)
+            .assert.containsText('@alertDanger', 'Puxa! não encontramos nada aqui.')
+    }
+}
